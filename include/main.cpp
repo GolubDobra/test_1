8218273dd8a55be254f9418ee50865b9e24ff066
Kolpakova Lera <11198lera@mail.ru>
2017-10-09 12:51:54 +0300

    Update main.cpp

--- include/main.cpp
+++ include/main.cpp
@@ -1,105 +1,68 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
-    Node* next;
+struct Node
+{
     int data;
+    Node* next;
 };
 
-template <class T>
-class Linked_List {
-private:
-    Node * head;
-    Node * tail;
+class List
+{
+    Node* Head;
 public:
-    Linked_List():head(0), tail(0){};
-    void push_back(T val);
-    void push_front(T val);
-    void reverse();
-    void display() const;
-    ~Linked_List();
+    List() 
+    {
+        Head = NULL; 
+    }
+    ~List();
+    void Add(int x);
+    void Show();
+    Node* reverse( Node * head);
 };
-template <class T>
-void Linked_List<T>::push_front(T val) {
-    Node * newNode = new Node;
 
-    newNode->data = val;
-    newNode->next = head;
-
-    head = newNode;
-
-    if(tail==0)
-        tail = head;
-}
-template <class T>
-void Linked_List<T>::push_back(T val) {
-    Node * newNode = new Node;
-
-    newNode->data = val;
-    newNode->next = 0;
-
-    if(tail==0)
+List::~List() 
+{
+    while(Head != NULL) 
     {
-        head = tail = newNode;
-        return;
+        Node* temp = Head->next;
+        delete Head;
+        Head = temp;
     }
-
-    tail->next = newNode;
-    tail = newNode;
 }
-template <class T>
-Linked_List<T>::~Linked_List() {
-    Node * tmp;
-    while(head)
+
+void List:: Add(int x) 
+{
+    Node* temp = new Node;
+    temp -> data = x;
+    temp -> next = Head;
+    Head = temp;
+}
+
+void List::Show()
+{
+    Node *temp = Head;
+    while(temp != NULL) 
     {
-        tmp = head;
-        head = head->next;
-        delete tmp;
+        cout << temp -> data << "";
+        temp = temp -> next;
     }
 }
-template <class T>
-void Linked_List<T>::display() const {
-    Node * curr = head;
 
-    while(curr) {
-        cout<<curr->data<<' ';
-        curr = curr->next;
+Node* List:: reverse(Node* head) 
+{
+    Node *reversed_list = NULL;
+    for (Node *node = head, *next_nod; node != NULL; node = next_nod) 
+    {
+        next_nod = node -> next;
+        node -> next = reversed_list;
+        reversed_list = node;
     }
-}
-template <class T>
-void Linked_List<T>::reverse() {
-    if (head == 0)
-        return;
-
-    tail = head;
-
-    Node * curr = head->next;
-    tail->next = 0;
-    Node * right;
-
-    while(curr) {
-        right = curr->next;
-        curr->next = head;
-        head = curr;
-        curr = right;
-    }
-
-    tail->next = 0;
+return reversed_list;
 }
 
-int main() {
-    Linked_List<int> intList;
-    intList.push_front(30);
-    intList.push_front(20);
-    intList.push_front(10);
-    intList.push_back(40);
-    intList.push_back(50);
-    intList.push_back(60);
-    cout << "Normal: ";
-    intList.display();
-    intList.reverse();
-    cout << "\nReversed: ";
-    intList.display();
-    cout << endl;
+int main()
+{
 
-return 0;
+    return 0;
+}
